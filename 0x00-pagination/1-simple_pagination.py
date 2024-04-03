db66740a6d42d1ff09d3641d325b92d8bd29d6ae
Muna-Redi <francischym@gmail.com>
2024-04-04 00:36:01 +0100

    delete resilient pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -30,7 +30,7 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self,page: int = 1, page_size: int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ returns the appropriate page of the dataset """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
