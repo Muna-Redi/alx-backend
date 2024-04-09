591a40d13724ab39f58d0dc14b422231d7ef31e4
Muna-Redi <francischym@gmail.com>
2024-04-09 19:41:28 +0100

    flask app

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""A simple flask app
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    ''' Language  configuration '''
+
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    ''' best-match language viaa user locale '''
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    ''' Babel usage route'''
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
